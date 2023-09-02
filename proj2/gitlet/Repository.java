7c6d6274cb72dfd1b4606b0b0df455b9e562e10f
lewiswon <lewis.won@gmail.com>
2023-09-02 11:28:39 +0800

    Added commit command

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -2,11 +2,11 @@ package gitlet;
 
 import javax.swing.*;
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 
 import static gitlet.Utils.*;
 
@@ -55,19 +55,27 @@ public class Repository {
         }
         GITLET_DIR.mkdir();
         Commit initialCommit = new Commit();
+
+        //Sha for initial commit
+        //Includes both the full and shortened sha
         String initialCommitSha = Utils.sha1(Utils.serialize(initialCommit));
         initialCommit.setThisId(initialCommitSha);
         Map<String, Commit> commitTree = new TreeMap<>();
         Map<String, String> commitTreeShortened = new TreeMap<>();
-        commitTree.put(initialCommitSha, initialCommit);
-        String initialCommitShaPart1 = initialCommitSha.substring(0,1);
-        String initialCommitShaPart2 = initialCommitSha.substring(2, initialCommitSha.length()-1);
+        Repository.writeToCommitTree(commitTree, commitTreeShortened, initialCommit);
+    }
+
+    private static void writeToCommitTree(Map<String, Commit> commitTree, Map<String, String> commitTreeShortened, Commit commitHead) {
+        String commitSha = Utils.sha1(Utils.serialize(commitHead));
+        commitTree.put(commitSha, commitHead);
+        String initialCommitShaPart1 = commitSha.substring(0,1);
+        String initialCommitShaPart2 = commitSha.substring(2, commitSha.length()-1);
         commitTreeShortened.put(initialCommitShaPart1, initialCommitShaPart2);
         File commitTreeFile = join(GITLET_DIR, "commitTree");
         File commitTreeShortenedFile = join(GITLET_DIR, "commitTreeShortened");
         writeObject(commitTreeFile, (Serializable) commitTree);
         writeObject(commitTreeShortenedFile, (Serializable) commitTreeShortened);
-        writeObject(COMMIT_HEAD, initialCommit);
+        writeObject(COMMIT_HEAD, commitHead);
     }
 
     public static void add(String fileName) {
@@ -101,7 +109,7 @@ public class Repository {
         /** read toAddFileMap */
         /** if toAddFileMap is empty, print "No changes added to the commit."  */
         Map<String, String> toAddFileMap = readObject(STAGING_DIR_MAP, TreeMap.class);
-        if (toAddFileMap.size() == 0) {
+        if (toAddFileMap.size() == 0 || !STAGING_DIR_MAP.exists()) {
             System.out.println("No changes added to the commit.");
             System.exit(0);
         }
@@ -117,24 +125,33 @@ public class Repository {
             File toCommitFile = join(COMMITTED_FILES_DIR, set.getKey(), set.getValue(), set.getKey());
             writeContents(toCommitFile, toCopyContent);
         }
+        commitHead = Repository.setCommitHead(commitHead, message);
+        File commitTreeFile = join(GITLET_DIR, "commitTree");
+        File commitTreeShortenedFile = join(GITLET_DIR, "commitTreeShortened");
+        Map<String, Commit> commitTree = readObject(commitTreeFile, TreeMap.class);
+        Map<String, String> commitTreeShortened = readObject(commitTreeShortenedFile, TreeMap.class);
+        Repository.writeToCommitTree(commitTree, commitTreeShortened, commitHead);
+
+        /** clear staging folder */
+        Utils.restrictedDelete(STAGING_DIR_MAP);
+        try {
+            Files.walk(STAGING_DIR.toPath())
+                    .sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .forEach(File::delete);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Commit setCommitHead(Commit commitHead, String message) {
         String commitHeadSha = sha1(commitHead);
         commitHead.setMessage(message);
         commitHead.setParentId(commitHead.thisId);
         commitHead.setThisId(commitHeadSha);
         commitHead.setTimestamp(new Date());
-        writeObject(COMMIT_HEAD, commitHead);
-        String commitHeadShaPart1 = commitHeadSha.substring(0,1);
-        String commitHeadShaPart2 = commitHeadSha.substring(2, commitHeadSha.length()-1);
-        File commitTreeFile = join(GITLET_DIR, "commitTree");
-        File commitTreeShortenedFile = join(GITLET_DIR, "commitTreeShortened");
-        Map<String, Commit> commitTree = readObject(commitTreeFile, TreeMap.class);
-        Map<String, String> commitTreeShortened = readObject(commitTreeShortenedFile, TreeMap.class);
-        commitTree.put(commitHeadSha, commitHead);
-        commitTreeShortened.put(commitHeadShaPart1, commitHeadShaPart2);
-        writeObject(commitTreeFile, (Serializable) commitTree);
-        writeObject(commitTreeShortenedFile, (Serializable) commitTreeShortened);
-        /** clear staging folder */
-
-
+        return commitHead;
     }
+
+
 }
